e9e7c0b1f4890ba8f51c9391423523d0b711f968
Mauricio9203 <mauriciogarrido1590@gmail.com>
2025-01-29 17:58:39 +1300

    CORS Implementation

--- index.py
+++ index.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, redirect, url_for, session
-from controllers.users import bp  as users_bp   # Importa el Blueprint de hola_mundo
-from controllers.login import login_bp  # Importa el Blueprint de login
+from flask_cors import CORS  # Importar CORS
+from controllers.users import bp as users_bp
+from controllers.login import login_bp
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mi_clave_secreta'  # Necesario para usar flash messages
+CORS(app, resources={r"/*": {"origins": "*"}})  # Habilitar CORS para todas las rutas
+
+app.config['SECRET_KEY'] = 'mi_clave_secreta'
 app.config['STATIC_FOLDER'] = 'static'
 
 # Registra los Blueprints
-app.register_blueprint(users_bp)  # Para 'hola_mundo'
-app.register_blueprint(login_bp)  # Para el login
+app.register_blueprint(users_bp)
+app.register_blueprint(login_bp)
 
 @app.route('/')
 def index():
-    # Verificar si el usuario está autenticado
     if 'email' not in session:
-        return redirect(url_for('login.login_get'))  # Redirige al login si no está autenticado
+        return redirect(url_for('login.login_get'))
     return render_template('index.html')
 
 @app.route('/users')
@@ -23,10 +25,9 @@ def usuarios_page():
 
 @app.route('/logout')
 def logout():
-    session.pop('email', None)  # Elimina el correo de la sesión
-    print("Sesión eliminada")  # Esto debería aparecer en la consola si la sesión se elimina correctamente
-    return redirect(url_for('login.login_get'))  # Redirige al login
-
+    session.pop('email', None)
+    print("Sesión eliminada")
+    return redirect(url_for('login.login_get'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
